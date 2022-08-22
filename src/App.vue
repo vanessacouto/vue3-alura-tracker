75b445bab6a3f4db3b80b6fb0b9f256cbdd3adce
Vanessa <vanessa-almeida@prodesan.com.br>
2022-08-22 15:41:14 -0300

    vue-router e a primeira rota

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,7 @@
       <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
     <div class="column is-three-quarter conteudo">
-      <Formulario @aoSalvarTarefa="salvarTarefa" />
-      <div class="lista">
-        <Tarefa
-          v-for="(tarefa, index) in tarefas"
-          :key="index"
-          :tarefa="tarefa"
-        />
-        <Box v-if="listaVazia">
-          Você não está muito produtivo hoje 😅 
-        </Box>
-      </div>
+      <router-view></router-view>
     </div>
   </main>
 </template>
@@ -22,36 +12,20 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import BarraLateral from "./components/BarraLateral.vue";
-import Formulario from "./components/Formulario.vue";
-import Tarefa from "./components/Tarefa.vue";
-import ITarefa from "./interfaces/ITarefa";
-import Box from "./components/Box.vue";
 
 // https://github.com/alura-cursos/alura-tracker
 
 export default defineComponent({
   name: "App",
   components: {
-    BarraLateral,
-    Formulario,
-    Tarefa,
-    Box,
+    BarraLateral
   },
   data() {
     return {
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false
     };
   },
-  computed: {
-    listaVazia (): boolean {
-      return this.tarefas.length === 0
-    }
-  },
   methods: {
-    salvarTarefa(tarefa: ITarefa) {
-      this.tarefas.push(tarefa);
-    },
     trocarTema (modoEscuroAtivo: boolean) {
       this.modoEscuroAtivo = modoEscuroAtivo
     }
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,42 @@
+<template>
+  <Formulario @aoSalvarTarefa="salvarTarefa" />
+  <div class="lista">
+    <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+    <Box v-if="listaVazia"> Você não está muito produtivo hoje 😅 </Box>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Formulario from "../components/Formulario.vue";
+import Tarefa from "../components/Tarefa.vue";
+import ITarefa from "../interfaces/ITarefa";
+import Box from "../components/Box.vue";
+
+// https://github.com/alura-cursos/alura-tracker
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Formulario,
+    Tarefa,
+    Box,
+  },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+    };
+  },
+  computed: {
+    listaVazia(): boolean {
+      return this.tarefas.length === 0;
+    },
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa);
+    },
+  },
+});
+</script>
+
