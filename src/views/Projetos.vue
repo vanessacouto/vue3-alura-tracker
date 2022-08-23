c6c986fa9e858fff56a7976f4276f33d3a5b6eef
Vanessa <vanessa-almeida@prodesan.com.br>
2022-08-23 10:18:45 -0300

    setup da store

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -33,27 +33,36 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import IProjeto from "../interfaces/IProjeto";
+import { useStore } from "@/store";
+import { computed, defineComponent } from "vue";
 
 export default defineComponent({
   name: "Projetos",
   data() {
     return {
-      nomeDoProjeto: "",
-      projetos: [] as IProjeto[],
+      nomeDoProjeto: ""
     };
   },
   methods: {
     salvar() {
-      const projeto: IProjeto = {
-        nome: this.nomeDoProjeto,
-        id: new Date().toISOString(),
-      };
-      this.projetos.push(projeto);
+      // const projeto: IProjeto = {
+      //   nome: this.nomeDoProjeto,
+      //   id: new Date().toISOString(),
+      // };
+      // this.projetos.push(projeto);
+
+      // salvar o projeto usando a mutation
+      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
       this.nomeDoProjeto = "";
     },
   },
+  setup () {
+    const store = useStore()
+    return {
+      store,
+      projetos: computed(() => store.state.projetos)
+    }
+  }
 });
 </script>
 
