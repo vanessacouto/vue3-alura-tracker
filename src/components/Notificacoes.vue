54bb2bf3967f1af23fd17f21731f199caa4021c8
Vanessa <vanessa-almeida@prodesan.com.br>
2022-08-24 09:58:10 -0300

    notificando via store

--- src/components/Notificacoes.vue
+++ src/components/Notificacoes.vue
@@ -1,25 +1,39 @@
 <template>
   <div class="notificacoes">
-    <article class="message is-success">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">aqui vai um texto de notificação</div>
-    </article>
-    <article class="message is-warning">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">aqui vai um texto de notificação</div>
-    </article>
-    <article class="message is-danger">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">aqui vai um texto de notificação</div>
+    <article
+      class="message"
+      :class="contexto[notificacao.tipo]"
+      v-for="notificacao in notificacoes"
+      :key="notificacao.id"
+    >
+      <div class="message-header">{{ notificacao.titulo }}</div>
+      <div class="message-body">{{ notificacao.texto }}</div>
     </article>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
+import { useStore } from "@/store";
+import { TipoNotificacao } from "@/interfaces/INotificacao";
 
 export default defineComponent({
   name: "Notificacoes",
+  data () {
+   return {
+       contexto: {
+           [TipoNotificacao.SUCESSO]: 'is-success',
+           [TipoNotificacao.ATENCAO]: 'is-warning',
+           [TipoNotificacao.FALHA]: 'is-danger'
+       }
+   }   
+  },
+  setup() {
+    const store = useStore();
+    return {
+      notificacoes: computed(() => store.state.notificacoes),
+    };
+  },
 });
 </script>
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,39 @@
+import INotificacao, { TipoNotificacao } from '@/interfaces/INotificacao'
 import IProjeto from '@/interfaces/IProjeto'
 import { InjectionKey } from 'vue'
 import { createStore, Store, useStore as vuexUseStore } from 'vuex'
 import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO } from './tipo-mutacoes'
 
 interface Estado {
-    projetos: IProjeto[]
+    projetos: IProjeto[],
+    notificacoes: INotificacao[]
 }
 
 export const key: InjectionKey<Store<Estado>> = Symbol()
 
 export const store = createStore<Estado>({
     state: {
-        projetos: []
+        projetos: [],
+        notificacoes: [
+            {
+                id: 1,
+                texto: 'Uma notificação de sucesso',
+                titulo: 'sucesso',
+                tipo: TipoNotificacao.SUCESSO
+            },
+            {
+                id: 2,
+                texto: 'Uma notificação de atenção',
+                titulo: 'atencao',
+                tipo: TipoNotificacao.ATENCAO
+            },
+            {
+                id: 3,
+                texto: 'Uma notificação de falha',
+                titulo: 'falha',
+                tipo: TipoNotificacao.FALHA
+            }
+        ]
     },
     mutations : {
         [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
