bb484b5e554ef799edd9a49bab414db036fdaa2d
Vanessa <vanessa-almeida@prodesan.com.br>
2022-08-25 16:20:58 -0300

    refatorando com composition API

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -35,7 +35,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
 import { useStore } from "vuex";
 import Temporizador from "./Temporizador.vue";
 
@@ -45,26 +45,29 @@ export default defineComponent({
   name: "Formulario",
   emits: ["aoSalvarTarefa"],
   components: { Temporizador },
-  data() {
-    return {
-      descricao: "",
-      idProjeto: ''
-    };
-  },
-  methods: {
-    finalizarTarefa(tempoDecorrido: number): void {
-      this.$emit("aoSalvarTarefa", {
-        duracaoEmSegundos: tempoDecorrido,
-        descricao: this.descricao,
-        projeto: this.projetos.find(proj => proj.id === this.idProjeto)
-      });
-      this.descricao = "";
-    },
-  },
-  setup() {
+  methods: {},
+  setup(props, { emit }) {
     const store = useStore(key);
+
+    const descricao = ref("");
+    const idProjeto = ref("");
+
+    const projetos = computed(() => store.state.projeto.projetos);
+
+    const finalizarTarefa = (tempoDecorrido: number): void => {
+      emit("aoSalvarTarefa", {
+        duracaoEmSegundos: tempoDecorrido,
+        descricao: descricao.value,
+        projeto: projetos.value.find((proj) => proj.id == idProjeto.value),
+      });
+      descricao.value = "";
+    };
+
     return {
-      projetos: computed(() => store.state.projeto.projetos),
+      descricao,
+      idProjeto,
+      projetos,
+      finalizarTarefa,
     };
   },
 });
