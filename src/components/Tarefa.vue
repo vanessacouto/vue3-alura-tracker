3adbe7b0ab233c75f20392445ce61b140ef5be86
Vanessa <vanessa-almeida@prodesan.com.br>
2022-08-25 13:35:15 -0300

    criando uma modal de edição

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,12 +1,14 @@
 <template>
   <Box class="box has-text-weight-bold">
-    <div class="columns">
-      <div class="column is-4">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
+    <div class="columns clicavel" @click="tarefaClicada">
+      <div class="column is-4">
+        {{ tarefa.descricao || "Tarefa sem descrição" }}
+      </div>
       <div class="column is-3">
-        {{ tarefa.projeto?.nome || 'N/D' }}
+        {{ tarefa.projeto?.nome || "N/D" }}
       </div>
       <div class="column is-7">
-        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
       </div>
     </div>
   </Box>
@@ -15,21 +17,32 @@
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import Cronometro from "./Cronometro.vue";
-import ITarefa from '../interfaces/ITarefa'
+import ITarefa from "../interfaces/ITarefa";
 import Box from "./Box.vue";
 
 export default defineComponent({
-    name: 'Tarefa',
-    components: {
-        Cronometro,
-        Box
+  name: "Tarefa",
+  emits: ["aoTarefaClicada"],
+  components: {
+    Cronometro,
+    Box,
+  },
+  methods: {
+    tarefaClicada(): void {
+      this.$emit("aoTarefaClicada", this.tarefa);
     },
-    props: {
-      tarefa: {
-        type: Object as PropType<ITarefa>,
-        required: true
-      }
-    }
+  },
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true,
+    },
+  },
 });
 </script>
 
+<style scoped>
+.clicavel {
+  cursor: pointer;
+}
+</style>
